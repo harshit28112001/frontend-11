9e0b2c37f476720a32eb4945514e59224d6d764f
Harshit Sikka <70819011+harshit28112001@users.noreply.github.com>
2024-02-08 21:37:48 +0530

    Update addblog.service.ts

--- src/app/Service/addblog.service.ts
+++ src/app/Service/addblog.service.ts
@@ -30,7 +30,7 @@ export class AddblogService {
     console.log(this.token)
     return headers;
   }
-  baseUrl = "http://dashboardharshit.itcblogs.xyz:8085/dashboard/addBlogs";
+  baseUrl = "http://dashboardharshit.itcblogs.xyz/dashboard/addBlogs";
 
   public addPosts(postData: any): Observable<Object> { 
     console.log(postData); 
@@ -38,7 +38,7 @@ export class AddblogService {
     return this.httpcl.post(this.baseUrl, postData);
   }
 
-  baseUrl1 = "http://dashboardharshit.itcblogs.xyz:8085/dashboard/showBlogs";
+  baseUrl1 = "http://dashboardharshit.itcblogs.xyz/dashboard/showBlogs";
 
   public getPosts() {
     const headers=new HttpHeaders({
