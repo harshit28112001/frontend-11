791907188a7f130ebe17105a5f97b687353a0715
Harshit Sikka <70819011+harshit28112001@users.noreply.github.com>
2024-02-08 21:37:12 +0530

    Update comment.service.ts

--- src/app/Service/comment.service.ts
+++ src/app/Service/comment.service.ts
@@ -10,10 +10,10 @@ export class CommentService {
   constructor(private blgs:AddblogService,private http:HttpClient) { }
   addComments(info:comment)
   {
-    return this.http.post(`http://dashboardharshit.itcblogs.xyz:8085/dashboard/addComment`,info)
+    return this.http.post(`http://dashboardharshit.itcblogs.xyz/dashboard/addComment`,info)
   }
   showComments(id:string)
   {
-    return this.http.get(`http://dashboardharshit.itcblogs.xyz:8085/dashboard/getComments/${id}`)
+    return this.http.get(`http://dashboardharshit.itcblogs.xyz/dashboard/getComments/${id}`)
   }
 }
